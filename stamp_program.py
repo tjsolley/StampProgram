df1f3f3340c233b6601754506d7e8b82825bcd7a
Tyler Solley <tjsolley1@gmail.com>
2020-09-13 18:47:12 -0700

    initial commit

---
+++ stamp_program.py
@@ -0,0 +1,24 @@
+"""
+start
+get the numbers of sheets
+sheets / 5
+round answer to next number
+output the result to the user
+end
+"""
+import math
+
+# input: sheet
+def calculate(sheet):
+  # step 1
+  answer = sheet / 5
+  # step 2
+  rounded = math.ceil(answer)
+  print("sheet is : ", sheet)
+  print("The answer is: ", answer)
+  print("rounded is: ", rounded)
+  print("=================================")
+  # output: number of stamps needed
+  return rounded
+
+output = calculate(16)
